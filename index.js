fd576602cde5ff4c6550c58b8d882fec4e5e029c
JPalmaI <josafatpalmavaldebenito@gmail.com>
2023-01-31 16:13:36 -0300

    subida del proyecto

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const workspaces = io.of(/^\/[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
+
+workspaces.on("connection", (socket) => {
+  console.log("usuario conectado");
+  socket.on("request_trama",()=>{
+    console.log("solicitaron datos");
+  })
+});
+
+
+
